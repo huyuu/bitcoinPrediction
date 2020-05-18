87232d2afe61cc2a2863324ac5b02f255e0b1e45
yuyang <huyuu258@gmail.com>
2020-05-18 22:05:29 +0900

    test 5.18.5

--- GaussianProcessAI.py
+++ GaussianProcessAI.py
@@ -1,11 +1,27 @@
 import numpy as nu
 import pandas as pd
+import multiprocessing as mp
 import sklearn as sk
 from sklearn import gaussian_process as gp
 import pickle
 from matplotlib import pyplot as pl
+import datetime as dt
 import os
-# import talib as ta
+import talib as ta
+
+from matplotlib.colors import ListedColormap
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_moons, make_circles, make_classification
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
 from plotGraph import plot
 
@@ -17,29 +33,59 @@ class GaussianProcessAI():
     def __init__(self, zoneLength=30, determinateSigma=1.0):
         self.zoneLength = zoneLength  # days
         self.determinateSigma = determinateSigma  # 1.5 * sigma
-        longtermKernel = 1.0 * gp.kernels.RBF(5.0)
-        shorttermKernel = 5.0 * gp.kernels.RationalQuadratic(length_scale=0.1, alpha=0.78)
-        noiseKernel = 0.1 * gp.kernels.WhiteKernel(1e-2)
-        kernel = longtermKernel + shorttermKernel + noiseKernel
-        self.model = gp.GaussianProcessClassifier(kernel=kernel, n_restarts_optimizer=9)
+        # longtermKernel = 1.0 * gp.kernels.RBF(5.0)
+        # shorttermKernel = 5.0 * gp.kernels.RationalQuadratic(length_scale=0.1, alpha=0.78)
+        # noiseKernel = 0.1 * gp.kernels.WhiteKernel(1e-2)
+        # kernel = longtermKernel + shorttermKernel + noiseKernel
+        # self.model = gp.GaussianProcessClassifier(kernel=1.0*gp.kernels.RBF(1.0), n_restarts_optimizer=9)
+        self.models = {
+            'GPC': gp.GaussianProcessClassifier(kernel=1.0*gp.kernels.RBF(1.0), n_restarts_optimizer=9),
+            'KNC': KNeighborsClassifier(3),
+            'SVC': SVC(gamma=2, C=1),
+            'RFC': RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+            'MLP': MLPClassifier(alpha=1, max_iter=1000),
+            'ABC': AdaBoostClassifier(),
+            'GNB': GaussianNB(),
+            'QDA': QuadraticDiscriminantAnalysis()
+        }
 
 
-    def varifyPrediction(self, testifyRadio=0.2):
+    def varifyPrediction(self):
         if os.path.exists(modelFileName):
             self.model = pickle.load(open(modelFileName, 'rb'))
         else:
-            self.trainFromHistoryData(testifyRadio)
+            self.trainFromHistoryData()
+        _BBsamples = nu.linspace(0, 1, 20)
+        _RSIsamples = nu.linspace(-2, 2, 20)
+        _BBsamplesGrid, _RSIsamplesGrid = nu.meshgrid(_BBsamples, _RSIsamples, indexing='ij')
+        samples = nu.concatenate([_BBsamplesGrid.reshape(-1, 1), _RSIsamplesGrid.reshape(-1, 1)], axis=1)
 
-        data = pd.read_csv(path).dropna()
-        data = data.head(data.index.values.shape[0] - self.zoneLength)
-        data['Delta'] = data['Close'] - data['Open']
-        amount = int(data.index.values.shape[0] * (1-testifyRadio))
-        samples = data['Delta'].values[amount:].reshape(-1, 1)
-        correctLabels = data['ClassLabel'].values[amount:].reshape(-1, 1)
+        probabilities = self.model.predict_proba(samples)
         predictedLabels = self.model.predict(samples)
-        pl.scatter(samples, predictedLabels, c='r')
-        pl.scatter(samples, correctLabels.ravel(), alpha=0.5)
+        data = pd.DataFrame(nu.concatenate([samples, predictedLabels.reshape(-1, 1), probabilities[:, 0].reshape(-1, 1), probabilities[:, 1].reshape(-1, 1), probabilities[:, 2].reshape(-1, 1)], axis=1), columns=['BBPosition', 'RSI14', 'ClassLabel', 'Label0_prob', 'Label1_prob', 'Label2_prob'])
+        print(data)
+        _0samples = data[data.ClassLabel == 0]
+        _1samples = data[data.ClassLabel == 1]
+        _2samples = data[data.ClassLabel == 2]
+        pl.scatter(_0samples['BBPosition'], _0samples['RSI14'], c='g')
+        pl.scatter(_1samples['BBPosition'], _1samples['RSI14'], c='gray', alpha=0.3)
+        pl.scatter(_2samples['BBPosition'], _2samples['RSI14'], c='r')
         pl.show()
+
+        ax = pl.axes(projection='3d')
+        ax.plot_surface(_BBsamplesGrid, _RSIsamplesGrid, data['Label0_prob'].values.reshape(20, 20), cmap='Blues')
+        pl.show()
+
+        fig = pl.figure()
+        ax = pl.axes(projection='3d')
+        ax.plot_surface(_BBsamplesGrid, _RSIsamplesGrid, data['Label1_prob'].values.reshape(20, 20), cmap='gray')
+        pl.show()
+
+        fig = pl.figure()
+        ax = pl.axes(projection='3d')
+        ax.plot_surface(_BBsamplesGrid, _RSIsamplesGrid, data['Label2_prob'].values.reshape(20, 20), cmap='Reds')
+        pl.show()
+
 
 
     def preprocessData(self):
@@ -114,27 +160,43 @@ class GaussianProcessAI():
         testSamples = data[['Delta', 'RSI14']].values[trainSamplesAmount:, :]
         testLabels = data['ClassLabel'].values[trainSamplesAmount:].reshape(-1, 1)
 
-        _0samples = data[data.ClassLabel == 0][['BBPosition', 'RSI14']]
-        _1samples = data[data.ClassLabel == 1][['BBPosition', 'RSI14']]
-        _2samples = data[data.ClassLabel == 2][['BBPosition', 'RSI14']]
+        # _0samples = data[data.ClassLabel == 0][['BBPosition', 'RSI14']]
+        # _1samples = data[data.ClassLabel == 1][['BBPosition', 'RSI14']]
+        # _2samples = data[data.ClassLabel == 2][['BBPosition', 'RSI14']]
         # pl.scatter(_0samples['BBPosition'], _0samples['RSI14'], c='g')
         # pl.scatter(_1samples['BBPosition'], _1samples['RSI14'], c='gray', alpha=0.3)
         # pl.scatter(_2samples['BBPosition'], _2samples['RSI14'], c='r')
         # pl.show()
         # start training
-        self.model.fit(trainSamples, trainLabels)
-        print(f'Model: {self.model.kernel_}')
-        print(f'Log_Marginal_Likelihood: {self.model.log_marginal_likelihood(self.model.kernel_.theta)}')
-        pickle.dump(self.model, open(modelFileName, 'wb'))
-        score = self.model.score(testSamples, testLabels)
-        print(score)
+        print('Start training model ...')
+        _start = dt.datetime.now()
+        # self.model.fit(trainSamples, trainLabels.ravel())
+        # print(f'Model: {self.model.kernel_}')
+        # print(f'Log_Marginal_Likelihood: {self.model.log_marginal_likelihood(self.model.kernel_.theta)}')
+        # pickle.dump(self.model, open(modelFileName, 'wb'))
+        # score = self.model.score(testSamples, testLabels)
+        # print(score)
 
+        processes = []
+        for name, model in self.models.items():
+            process = mp.Process(target=trainModel, args=(name, model, trainSamples, trainLabels, testSamples, testLabels))
+            process.start()
+            processes.append(process)
+        for process in processes:
+            process.join()
+        timeCost = (dt.datetime.now() - _start).total_seconds()
+        print(f'Model training ends. (time cost: {timeCost} seconds)')
+
+
+def trainModel(name, model, trainSamples, trainLabels, testSamples, testLabels):
+    model.fit(trainSamples, trainLabels.ravel())
+    score = model.score(testSamples, testLabels.ravel())
+    print(f'{name} model score: {score}')
 
 
 if __name__ == '__main__':
     ai = GaussianProcessAI()
     # ai.preprocessData()
     ai.trainFromHistoryData()
-
     # ai.varifyPrediction()
     # plot()
