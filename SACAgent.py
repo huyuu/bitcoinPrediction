bc51c55e15b9ed979251303c01b0a405f8640b6f
yuyang <huyuu258@gmail.com>
2020-08-22 12:54:51 +0900

    SAC Agent update 12

--- SACAgent.py
+++ SACAgent.py
@@ -6,6 +6,7 @@ from __future__ import print_function
 import numpy as nu
 import pandas as pd
 import datetime as dt
+import multiprocessing as mp
 # Tensorflow Modules
 import tensorflow as tf
 tf.compat.v1.enable_v2_behavior()
@@ -26,58 +27,92 @@ from BitcoinEnvironment import BTC_JPY_Environment
 
 
 
-# Environment
+if __name__ == '__main__':
+    mp.freeze_support()
+    # Environment
 
-# create environment and transfer it to Tensorflow version
-print('Creating environment ...')
-env = BTC_JPY_Environment(imageWidth=int(24*4), imageHeight=int(24*8), initialAsset=100000, isHugeMemorryMode=True)
-env = tf_py_environment.TFPyEnvironment(env)
-evaluate_env = tf_py_environment.TFPyEnvironment(BTC_JPY_Environment(imageWidth=int(24*4), imageHeight=int(24*8), initialAsset=100000, isHugeMemorryMode=False))
-observation_spec = env.observation_spec()
-action_spec = env.action_spec()
-print('Environment created.')
-
-
-# Hyperparameters
-
-batchSize = int(4*24)
-
-criticLearningRate = 3e-4
-actorLearningRate = 3e-4
-alphaLearningRate = 3e-4
-
-gamma = 0.999
-gradientClipping = None
-target_update_tau = 0.005
-
-# (num_units, kernel_size, stride)
-# critic_observationConvLayerParams = [(24, 3, 1), (24, 3, 1)]
-# critic_observationDenseLayerParams = [int(env.observation_spec()[0].shape[0]//2), int(env.observation_spec()[0].shape[0]//2)]
-critic_commonDenseLayerParams = [int(observation_spec[0].shape[0]//2)]
-# actor_convLayerParams = [(96, 3, 1), (24, 3, 1)]
-actor_denseLayerParams = [int(observation_spec[0].shape[0]//2)]
-
-_storeYears = 2
-replayBufferCapacity = int(batchSize * _storeYears)
-warmupEpisodes = int(_storeYears * 4)
-validateEpisodes = 4
+    # create environment and transfer it to Tensorflow version
+    print('Creating environment ...')
+    env = BTC_JPY_Environment(imageWidth=int(24*4), imageHeight=int(24*8), initialAsset=100000, isHugeMemorryMode=True)
+    env = tf_py_environment.TFPyEnvironment(env)
+    evaluate_env = tf_py_environment.TFPyEnvironment(BTC_JPY_Environment(imageWidth=int(24*4), imageHeight=int(24*8), initialAsset=100000, isHugeMemorryMode=False))
+    observation_spec = env.observation_spec()
+    action_spec = env.action_spec()
+    print('Environment created.')
 
 
+    # Hyperparameters
 
-# Models
+    batchSize = int(4*24)
 
-# create Crite Network
-# https://www.tensorflow.org/agents/api_docs/python/tf_agents/agents/ddpg/critic_network/CriticNetwork
-# critic_net = critic_network.CriticNetwork(
-#     (observation_spec, action_spec),
-#     observation_fc_layer_params=critic_observationDenseLayerParams,
-#     action_fc_layer_params=None,
-#     joint_fc_layer_params=critic_commonDenseLayerParams
-# )
-critic_net = value_network.ValueNetwork(
-    (observation_spec, action_spec),
-    preprocessing_layers=(
-        (
+    criticLearningRate = 3e-4
+    actorLearningRate = 3e-4
+    alphaLearningRate = 3e-4
+
+    gamma = 0.999
+    gradientClipping = None
+    target_update_tau = 0.005
+
+    # (num_units, kernel_size, stride)
+    # critic_observationConvLayerParams = [(24, 3, 1), (24, 3, 1)]
+    # critic_observationDenseLayerParams = [int(env.observation_spec()[0].shape[0]//2), int(env.observation_spec()[0].shape[0]//2)]
+    critic_commonDenseLayerParams = [int(observation_spec[0].shape[0]//2)]
+    # actor_convLayerParams = [(96, 3, 1), (24, 3, 1)]
+    actor_denseLayerParams = [int(observation_spec[0].shape[0]//2)]
+
+    _storeYears = 2
+    replayBufferCapacity = int(batchSize * _storeYears)
+    warmupEpisodes = int(_storeYears * 4)
+    validateEpisodes = 4
+
+
+
+    # Models
+
+    # create Crite Network
+    # https://www.tensorflow.org/agents/api_docs/python/tf_agents/agents/ddpg/critic_network/CriticNetwork
+    # critic_net = critic_network.CriticNetwork(
+    #     (observation_spec, action_spec),
+    #     observation_fc_layer_params=critic_observationDenseLayerParams,
+    #     action_fc_layer_params=None,
+    #     joint_fc_layer_params=critic_commonDenseLayerParams
+    # )
+    critic_net = value_network.ValueNetwork(
+        (observation_spec, action_spec),
+        preprocessing_layers=(
+            (
+                kr.models.Sequential([
+                    kr.layers.Conv2D(filters=int((observation_spec[0].shape[0]*observation_spec[0].shape[1])//3), kernel_size=3, activation='relu', input_shape=(observation_spec[0].shape[0], observation_spec[0].shape[1], 1)),
+                    kr.layers.Conv2D(filters=int((observation_spec[0].shape[0]*observation_spec[0].shape[1])//3), kernel_size=3, activation='relu', input_shape=(observation_spec[0].shape[0], observation_spec[0].shape[1], 1)),
+                    kr.layers.Flatten()
+                ]),
+                kr.layers.Dense(1, activation='sigmoid')
+            ),
+            kr.layers.Dense(1, activation='sigmoid')
+        ),
+        preprocessing_combiner=kr.layers.Concatenate(axis=-1),
+        conv_layer_params=None,
+        fc_layer_params=critic_commonDenseLayerParams,
+        dtype=tf.float32,
+        name='Critic Network'
+    )
+    print('Critic Network Created.')
+
+    # create Actor Network
+    def normal_projection_net(action_spec):
+        return normal_projection_network.NormalProjectionNetwork(
+            action_spec,
+            mean_transform=None,
+            state_dependent_std=True,
+            init_means_output_factor=0.1,
+            std_transform=sac_agent.std_clip_transform,
+            scale_distribution=True
+        )
+    # https://www.tensorflow.org/agents/api_docs/python/tf_agents/networks/actor_distribution_network/ActorDistributionNetwork
+    actor_net = actor_distribution_network.ActorDistributionNetwork(
+        input_tensor_spec=observation_spec,
+        output_tensor_spec=action_spec,
+        preprocessing_layers=(
             kr.models.Sequential([
                 kr.layers.Conv2D(filters=int((observation_spec[0].shape[0]*observation_spec[0].shape[1])//3), kernel_size=3, activation='relu', input_shape=(observation_spec[0].shape[0], observation_spec[0].shape[1], 1)),
                 kr.layers.Conv2D(filters=int((observation_spec[0].shape[0]*observation_spec[0].shape[1])//3), kernel_size=3, activation='relu', input_shape=(observation_spec[0].shape[0], observation_spec[0].shape[1], 1)),
@@ -85,136 +120,104 @@ critic_net = value_network.ValueNetwork(
             ]),
             kr.layers.Dense(1, activation='sigmoid')
         ),
-        kr.layers.Dense(1, activation='sigmoid')
-    ),
-    preprocessing_combiner=kr.layers.Concatenate(axis=-1),
-    conv_layer_params=None,
-    fc_layer_params=critic_commonDenseLayerParams,
-    dtype=tf.float32,
-    name='Critic Network'
-)
-print('Critic Network Created.')
-
-# create Actor Network
-def normal_projection_net(action_spec):
-    return normal_projection_network.NormalProjectionNetwork(
-        action_spec,
-        mean_transform=None,
-        state_dependent_std=True,
-        init_means_output_factor=0.1,
-        std_transform=sac_agent.std_clip_transform,
-        scale_distribution=True
+        preprocessing_combiner=kr.layers.Concatenate(axis=-1),
+        fc_layer_params=actor_denseLayerParams,
+        dtype=tf.float32,
+        continuous_projection_net=normal_projection_net,
+        name='ActorDistributionNetwork'
     )
-# https://www.tensorflow.org/agents/api_docs/python/tf_agents/networks/actor_distribution_network/ActorDistributionNetwork
-actor_net = actor_distribution_network.ActorDistributionNetwork(
-    input_tensor_spec=observation_spec,
-    output_tensor_spec=action_spec,
-    preprocessing_layers=(
-        kr.models.Sequential([
-            kr.layers.Conv2D(filters=int((observation_spec[0].shape[0]*observation_spec[0].shape[1])//3), kernel_size=3, activation='relu', input_shape=(observation_spec[0].shape[0], observation_spec[0].shape[1], 1)),
-            kr.layers.Conv2D(filters=int((observation_spec[0].shape[0]*observation_spec[0].shape[1])//3), kernel_size=3, activation='relu', input_shape=(observation_spec[0].shape[0], observation_spec[0].shape[1], 1)),
-            kr.layers.Flatten()
-        ]),
-        kr.layers.Dense(1, activation='sigmoid')
-    ),
-    preprocessing_combiner=kr.layers.Concatenate(axis=-1),
-    fc_layer_params=actor_denseLayerParams,
-    dtype=tf.float32,
-    continuous_projection_net=normal_projection_net,
-    name='ActorDistributionNetwork'
-)
-print('Actor Network Created.')
+    print('Actor Network Created.')
 
-# create SAC Agent
-# https://www.tensorflow.org/agents/api_docs/python/tf_agents/agents/SacAgent
-global_step = tf.compat.v1.train.get_or_create_global_step()
-tf_agent = sac_agent.SacAgent(
-    env.time_step_spec(),
-    action_spec,
-    actor_network=actor_net,
-    critic_network=critic_net,
-    actor_optimizer=tf.compat.v1.train.AdamOptimizer(learning_rate=actorLearningRate),
-    critic_optimizer=tf.compat.v1.train.AdamOptimizer(learning_rate=criticLearningRate),
-    alpha_optimizer=tf.compat.v1.train.AdamOptimizer(learning_rate=alphaLearningRate),
-    target_update_tau=target_update_tau,
-    td_errors_loss_fn=tf.compat.v1.losses.mean_squared_error,
-    gamma=gamma,
-    gradient_clipping=gradientClipping,
-    train_step_counter=global_step
-)
-tf_agent.initialize()
-print('SAC Agent Created.')
+    # create SAC Agent
+    # https://www.tensorflow.org/agents/api_docs/python/tf_agents/agents/SacAgent
+    global_step = tf.compat.v1.train.get_or_create_global_step()
+    tf_agent = sac_agent.SacAgent(
+        env.time_step_spec(),
+        action_spec,
+        actor_network=actor_net,
+        critic_network=critic_net,
+        actor_optimizer=tf.compat.v1.train.AdamOptimizer(learning_rate=actorLearningRate),
+        critic_optimizer=tf.compat.v1.train.AdamOptimizer(learning_rate=criticLearningRate),
+        alpha_optimizer=tf.compat.v1.train.AdamOptimizer(learning_rate=alphaLearningRate),
+        target_update_tau=target_update_tau,
+        td_errors_loss_fn=tf.compat.v1.losses.mean_squared_error,
+        gamma=gamma,
+        gradient_clipping=gradientClipping,
+        train_step_counter=global_step
+    )
+    tf_agent.initialize()
+    print('SAC Agent Created.')
 
-# policies
-evaluate_policy = greedy_policy.GreedyPolicy(tf_agent.policy)
-collect_policy = tf_agent.collect_policy
+    # policies
+    evaluate_policy = greedy_policy.GreedyPolicy(tf_agent.policy)
+    collect_policy = tf_agent.collect_policy
 
-# metrics and evaluation
-def compute_avg_return(environment, policy, num_episodes=5):
-    total_return = 0.0
-    for _ in range(num_episodes):
-        time_step = environment.reset()
-        episode_return = 0.0
-        while not time_step.is_last():
-            action_step = policy.action(time_step)
-            time_step = environment.step(action_step.action)
-            episode_return += time_step.reward
-        total_return += episode_return
-    avg_return = total_return / num_episodes
-    return avg_return.numpy()[0]
+    # metrics and evaluation
+    def compute_avg_return(environment, policy, num_episodes=5):
+        total_return = 0.0
+        for _ in range(num_episodes):
+            time_step = environment.reset()
+            episode_return = 0.0
+            while not time_step.is_last():
+                action_step = policy.action(time_step)
+                time_step = environment.step(action_step.action)
+                episode_return += time_step.reward
+            total_return += episode_return
+        avg_return = total_return / num_episodes
+        return avg_return.numpy()[0]
 
-# create Reply Buffer
-# https://www.tensorflow.org/agents/tutorials/5_replay_buffers_tutorial
-# https://www.google.com/url?client=internal-element-cse&cx=016807462989910793636:iigazrvgr1m&q=https://www.tensorflow.org/agents/api_docs/python/tf_agents/replay_buffers/tf_uniform_replay_buffer/TFUniformReplayBuffer&sa=U&ved=2ahUKEwivq9qvnaTrAhXMdXAKHf2nBQYQFjAAegQIBBAB&usg=AOvVaw2elqEhFKUSZf8WeAl53gVK
-replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
-    data_spec=tf_agent.collect_data_spec,
-    batch_size=env.batch_size,
-    max_length=replayBufferCapacity
-)
-print('Replay Buffer Created, start warming-up ...')
+    # create Reply Buffer
+    # https://www.tensorflow.org/agents/tutorials/5_replay_buffers_tutorial
+    # https://www.google.com/url?client=internal-element-cse&cx=016807462989910793636:iigazrvgr1m&q=https://www.tensorflow.org/agents/api_docs/python/tf_agents/replay_buffers/tf_uniform_replay_buffer/TFUniformReplayBuffer&sa=U&ved=2ahUKEwivq9qvnaTrAhXMdXAKHf2nBQYQFjAAegQIBBAB&usg=AOvVaw2elqEhFKUSZf8WeAl53gVK
+    replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
+        data_spec=tf_agent.collect_data_spec,
+        batch_size=env.batch_size,
+        max_length=replayBufferCapacity
+    )
+    print('Replay Buffer Created, start warming-up ...')
 
-# driver for warm-up
-# https://www.tensorflow.org/agents/api_docs/python/tf_agents/drivers/dynamic_episode_driver/DynamicEpisodeDriver
-initial_collect_driver = dynamic_episode_driver.DynamicEpisodeDriver(
-    env,
-    collect_policy,
-    observers=[replay_buffer.add_batch],
-    num_episodes=warmupEpisodes
-)
-initial_collect_driver.run()
-print('Replay Buffer Warm-up Done.')
+    # driver for warm-up
+    # https://www.tensorflow.org/agents/api_docs/python/tf_agents/drivers/dynamic_episode_driver/DynamicEpisodeDriver
+    initial_collect_driver = dynamic_episode_driver.DynamicEpisodeDriver(
+        env,
+        collect_policy,
+        observers=[replay_buffer.add_batch],
+        num_episodes=warmupEpisodes
+    )
+    initial_collect_driver.run()
+    print('Replay Buffer Warm-up Done.')
 
 
-# Training
+    # Training
 
-collect_driver = dynamic_episode_driver.DynamicEpisodeDriver(
-    env,
-    collect_policy,
-    observers=[replay_buffer.add_batch],
-    num_episodes=1
-)
-# (Optional) Optimize by wrapping some of the code in a graph using TF function.
-tf_agent.train = common.function(tf_agent.train)
-collect_driver.run = common.function(collect_driver.run)
-# Reset the train step
-tf_agent.train_step_counter.assign(0)
-# Evaluate the agent's policy once before training.
-avg_return = compute_avg_return(evaluate_env, evaluate_policy, validateEpisodes)
-returns = [avg_return]
-# Main training process
-dataset = replay_buffer.as_dataset(num_parallel_calls=3, sample_batch_size=batchSize, num_steps=2).prefetch(3)
-iterator = iter(dataset)
-print('Start training...')
-for _ in range(num_iterations):
-    # Collect a few steps using collect_policy and save to the replay buffer.
-    collect_driver.run()
-    # Sample a batch of data from the buffer and update the agent's network.
-    experience, unused_info = next(iterator)
-    train_loss = tf_agent.train(experience)
-    step = tf_agent.train_step_counter.numpy()
-    if step % log_interval == 0:
-        print('step = {0}: loss = {1}'.format(step, train_loss.loss))
-    if step % eval_interval == 0:
-        avg_return = compute_avg_return(evaluate_env, evaluate_policy, num_eval_episodes)
-        print('step = {0}: Average Return = {1}'.format(step, avg_return))
-    returns.append(avg_return)
+    collect_driver = dynamic_episode_driver.DynamicEpisodeDriver(
+        env,
+        collect_policy,
+        observers=[replay_buffer.add_batch],
+        num_episodes=1
+    )
+    # (Optional) Optimize by wrapping some of the code in a graph using TF function.
+    tf_agent.train = common.function(tf_agent.train)
+    collect_driver.run = common.function(collect_driver.run)
+    # Reset the train step
+    tf_agent.train_step_counter.assign(0)
+    # Evaluate the agent's policy once before training.
+    avg_return = compute_avg_return(evaluate_env, evaluate_policy, validateEpisodes)
+    returns = [avg_return]
+    # Main training process
+    dataset = replay_buffer.as_dataset(num_parallel_calls=3, sample_batch_size=batchSize, num_steps=2).prefetch(3)
+    iterator = iter(dataset)
+    print('Start training...')
+    for _ in range(num_iterations):
+        # Collect a few steps using collect_policy and save to the replay buffer.
+        collect_driver.run()
+        # Sample a batch of data from the buffer and update the agent's network.
+        experience, unused_info = next(iterator)
+        train_loss = tf_agent.train(experience)
+        step = tf_agent.train_step_counter.numpy()
+        if step % log_interval == 0:
+            print('step = {0}: loss = {1}'.format(step, train_loss.loss))
+        if step % eval_interval == 0:
+            avg_return = compute_avg_return(evaluate_env, evaluate_policy, num_eval_episodes)
+            print('step = {0}: Average Return = {1}'.format(step, avg_return))
+        returns.append(avg_return)
