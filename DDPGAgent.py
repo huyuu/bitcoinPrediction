304d21493dbd2ea7de26a795eab29d551149edd7
yuyang <huyuu258@gmail.com>
2020-09-29 14:54:47 +0900

    SAC 0.41

---
+++ DDPGAgent.py
@@ -0,0 +1,205 @@
+# Importing
+# Python Modules
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import numpy as nu
+import pandas as pd
+import datetime as dt
+import multiprocessing as mp
+from matplotlib import pyplot as pl
+# Tensorflow Modules
+import tensorflow as tf
+tf.compat.v1.enable_v2_behavior()
+from tensorflow import keras as kr
+from tf_agents.networks.network import Network
+from tf_agents.agents.ddpg import critic_network, DdpgAgent
+from tf_agents.agents.ddpg.actor_network import ActorNetwork
+from tf_agents.agents.ddpg.critic_network import CriticNetwork
+from tf_agents.agents.sac import sac_agent, tanh_normal_projection_network
+from tf_agents.drivers import dynamic_step_driver, dynamic_episode_driver
+from tf_agents.environments import tf_py_environment
+from tf_agents.experimental.train import actor, learner, triggers
+from tf_agents.experimental.train.utils import spec_utils, strategy_utils, train_utils
+from tf_agents.eval import metric_utils
+from tf_agents.metrics import tf_metrics
+from tf_agents.networks import actor_distribution_network, normal_projection_network, value_network, q_network
+from tf_agents.policies import greedy_policy, random_tf_policy
+from tf_agents.replay_buffers import tf_uniform_replay_buffer, reverb_replay_buffer, reverb_utils
+from tf_agents.trajectories import trajectory
+from tf_agents.utils import common
+# Custom Modules
+from BitcoinEnvironment import BTC_JPY_Environment
+
+
+if __name__ == '__main__':
+    mp.freeze_support()
+    # Environment
+
+    # create environment and transfer it to Tensorflow version
+    print('Creating environment ...')
+    env = BTC_JPY_Environment(imageWidth=int(24*4), imageHeight=int(24*8), initialAsset=100000, isHugeMemorryMode=True)
+    env = tf_py_environment.TFPyEnvironment(env)
+    evaluate_env = tf_py_environment.TFPyEnvironment(BTC_JPY_Environment(imageWidth=int(24*4), imageHeight=int(24*8), initialAsset=100000, isHugeMemorryMode=False))
+    observation_spec = env.observation_spec()
+    action_spec = env.action_spec()
+    print('Environment created.')
+
+    # Hyperparameters
+    critic_commonDenseLayerParams = [int(observation_spec['observation_market'].shape[0]//4)]
+    gamma = 0.99
+    batchSize = 1
+    target_update_tau = 0.005
+
+    critic_observationDenseLayerParams = [int(observation_spec['observation_market'].shape[0]//4)]
+    critic_commonDenseLayerParams = [int(observation_spec['observation_market'].shape[0]//4)]
+    # actor_convLayerParams = [(96, 3, 1), (24, 3, 1)]
+    actor_denseLayerParams = [int(observation_spec['observation_market'].shape[0]//4)]
+
+    _storeYears = 2
+    replayBufferCapacity = int(_storeYears * 4 * 3 * 30 * 24 * 4)
+    warmupEpisodes = int(_storeYears * 4)
+    validateEpisodes = 2
+
+    num_iterations = 300
+    log_interval = 1000
+    eval_interval = 10000
+
+    # Actor
+    actor_net = actor_distribution_network.ActorDistributionNetwork(
+        input_tensor_spec=observation_spec,
+        output_tensor_spec=action_spec,
+        preprocessing_layers={
+            'observation_market': kr.models.Sequential([
+                kr.layers.Conv2D(filters=int((observation_spec['observation_market'].shape[0]*observation_spec['observation_market'].shape[1])//8), kernel_size=3, activation='relu', input_shape=(observation_spec['observation_market'].shape[0], observation_spec['observation_market'].shape[1], 1)),
+                # kr.layers.Conv2D(filters=int((observation_spec[0].shape[0]*observation_spec[0].shape[1])//8), kernel_size=3, activation='relu', input_shape=(observation_spec[0].shape[0], observation_spec[0].shape[1], 1)),
+                kr.layers.Flatten()
+            ]),
+            'observation_holdingRate': kr.layers.Dense(1, activation='sigmoid')
+        },
+        preprocessing_combiner=kr.layers.Concatenate(axis=-1),
+        fc_layer_params=actor_denseLayerParams,
+        dtype=tf.float32,
+        continuous_projection_net=tanh_normal_projection_network.TanhNormalProjectionNetwork,
+        name='ActorDistributionNetwork'
+    )
+    print('Actor Network Created.')
+    # Critic Network
+    critic_net = value_network.ValueNetwork(
+        (observation_spec, action_spec),
+        preprocessing_layers=(
+            {
+                'observation_market': kr.models.Sequential([
+                    kr.layers.Conv2D(filters=int((observation_spec['observation_market'].shape[0]*observation_spec['observation_market'].shape[1])//8), kernel_size=3, activation='relu', input_shape=(observation_spec['observation_market'].shape[0], observation_spec['observation_market'].shape[1], 1)),
+                    # kr.layers.Conv2D(filters=int((observation_spec[0].shape[0]*observation_spec[0].shape[1])//8), kernel_size=3, activation='relu', input_shape=(observation_spec[0].shape[0], observation_spec[0].shape[1], 1)),
+                    kr.layers.Flatten()
+                ]),
+                'observation_holdingRate': kr.layers.Dense(1, activation='sigmoid')
+            },
+            kr.layers.Dense(1, activation='sigmoid')
+        ),
+        preprocessing_combiner=kr.layers.Concatenate(axis=-1),
+        conv_layer_params=None,
+        fc_layer_params=critic_commonDenseLayerParams,
+        dtype=tf.float32,
+        name='Critic Network'
+    )
+    print('Critic Network Created.')
+    # DDPG Agent
+    global_step = tf.compat.v1.train.get_or_create_global_step()
+    DdpgAgent(
+        time_step_spec: tf_agents.trajectories.time_step.TimeStep,
+        action_spec: action_spec,
+        actor_network: actor_network,
+        critic_network: critic_network,
+        dqda_clipping: None,
+        td_errors_loss_fn: None,
+        gamma: gamma,
+        reward_scale_factor: 1.0,
+        gradient_clipping: None,
+        summarize_grads_and_vars: bool = False,
+        train_step_counter: global_step,
+        name: 'Agent'
+    )
+
+    # policies
+    evaluate_policy = greedy_policy.GreedyPolicy(tf_agent.policy)
+    collect_policy = tf_agent.collect_policy
+
+    # metrics and evaluation
+    def compute_avg_return(environment, policy, num_episodes=5):
+        total_return = 0.0
+        for _ in range(num_episodes):
+            time_step = environment.reset()
+            episode_return = 0.0
+            while not time_step.is_last():
+                action_step = policy.action(time_step)
+                time_step = environment.step(action_step.action)
+                episode_return += time_step.reward
+            total_return += episode_return
+        avg_return = total_return / num_episodes
+        return avg_return.numpy()[0]
+
+    # create Reply Buffer
+    # https://www.tensorflow.org/agents/tutorials/5_replay_buffers_tutorial
+    # https://www.google.com/url?client=internal-element-cse&cx=016807462989910793636:iigazrvgr1m&q=https://www.tensorflow.org/agents/api_docs/python/tf_agents/replay_buffers/tf_uniform_replay_buffer/TFUniformReplayBuffer&sa=U&ved=2ahUKEwivq9qvnaTrAhXMdXAKHf2nBQYQFjAAegQIBBAB&usg=AOvVaw2elqEhFKUSZf8WeAl53gVK
+    replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
+        data_spec=tf_agent.collect_data_spec,
+        batch_size=batchSize,
+        max_length=replayBufferCapacity
+    )
+    print(tf_agent.collect_data_spec)
+    print('Replay Buffer Created, start warming-up ...')
+    _startTime = dt.datetime.now()
+
+    # driver for warm-up
+    # https://www.tensorflow.org/agents/api_docs/python/tf_agents/drivers/dynamic_episode_driver/DynamicEpisodeDriver
+    initial_collect_driver = dynamic_episode_driver.DynamicEpisodeDriver(
+        env,
+        collect_policy,
+        observers=[replay_buffer.add_batch],
+        num_episodes=warmupEpisodes
+    )
+    initial_collect_driver.run()
+    _timeCost = (dt.datetime.now() - _startTime).total_seconds()
+    print('Replay Buffer Warm-up Done. (cost {:.3g} hours)'.format(_timeCost/3600.0))
+    _startTime = dt.datetime.now()
+
+
+    # Training
+
+    print('Prepare for training ...')
+    collect_driver = dynamic_episode_driver.DynamicEpisodeDriver(
+        env,
+        collect_policy,
+        observers=[replay_buffer.add_batch],
+        num_episodes=1
+    )
+    # # (Optional) Optimize by wrapping some of the code in a graph using TF function.
+    # tf_agent.train = common.function(tf_agent.train)
+    # collect_driver.run = common.function(collect_driver.run)
+    # Reset the train step
+    tf_agent.train_step_counter.assign(0)
+    # Evaluate the agent's policy once before training.
+    avg_return = compute_avg_return(evaluate_env, evaluate_policy, validateEpisodes)
+    returns = [avg_return]
+    # Main training process
+    dataset = replay_buffer.as_dataset(num_parallel_calls=7, sample_batch_size=batchSize, num_steps=2)
+    iterator = iter(dataset)
+    _timeCost = (dt.datetime.now() - _startTime).total_seconds()
+    print('All preparation is done (cost {:.3g} hours). Start training...'.format(_timeCost/3600.0))
+    for _ in range(num_iterations):
+        # Collect a few steps using collect_policy and save to the replay buffer.
+        collect_driver.run()
+        # Sample a batch of data from the buffer and update the agent's network.
+        experience, unused_info = next(iterator)
+        train_loss = tf_agent.train(experience)
+        step = tf_agent.train_step_counter.numpy()
+        # if step % log_interval == 0:
+        print('step = {0}: loss = {1}'.format(step, train_loss.loss))
+        # if step % eval_interval == 0:
+        avg_return = compute_avg_return(evaluate_env, evaluate_policy, validateEpisodes)
+        print('step = {0}: Average Return = {1}'.format(step, avg_return))
+        returns.append(avg_return)
+    pl.plot(returns)
+    pl.show()
