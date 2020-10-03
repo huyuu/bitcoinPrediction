36841fe5d2789a04d2415c481f1f4fde54180f66
yuyang <huyuu258@gmail.com>
2020-10-03 10:57:29 +0900

    A2C Agent 1.7

--- A2CAgent.py
+++ A2CAgent.py
@@ -87,10 +87,14 @@ if __name__ == '__main__':
         input_tensor_spec=observation_spec,
         output_tensor_spec=action_spec,
         preprocessing_layers={
-            'observation_market': kr.models.Sequential([
+            # 'observation_market': kr.models.Sequential([
+            #     kr.layers.Conv2D(filters=int((observation_spec['observation_market'].shape[0]*observation_spec['observation_market'].shape[1])//100), kernel_size=3, activation='relu', input_shape=(observation_spec['observation_market'].shape[0], observation_spec['observation_market'].shape[1], 1)),
+            #     kr.layers.Flatten()
+            # ]),
+            'observation_market': [
                 kr.layers.Conv2D(filters=int((observation_spec['observation_market'].shape[0]*observation_spec['observation_market'].shape[1])//100), kernel_size=3, activation='relu', input_shape=(observation_spec['observation_market'].shape[0], observation_spec['observation_market'].shape[1], 1)),
                 kr.layers.Flatten()
-            ]),
+            ],
             'observation_holdingRate': kr.layers.Dense(1, activation='tanh')
         },
         preprocessing_combiner=kr.layers.Concatenate(axis=-1),
@@ -105,10 +109,14 @@ if __name__ == '__main__':
     critic_net = ValueNetwork(
         observation_spec,
         preprocessing_layers={
-            'observation_market': kr.models.Sequential([
+            # 'observation_market': kr.models.Sequential([
+            #     kr.layers.Conv2D(filters=int((observation_spec['observation_market'].shape[0]*observation_spec['observation_market'].shape[1])//100), kernel_size=3, activation='relu', input_shape=(observation_spec['observation_market'].shape[0], observation_spec['observation_market'].shape[1], 1)),
+            #     kr.layers.Flatten()
+            # ]),
+            'observation_market': [
                 kr.layers.Conv2D(filters=int((observation_spec['observation_market'].shape[0]*observation_spec['observation_market'].shape[1])//100), kernel_size=3, activation='relu', input_shape=(observation_spec['observation_market'].shape[0], observation_spec['observation_market'].shape[1], 1)),
                 kr.layers.Flatten()
-            ]),
+            ],
             'observation_holdingRate': kr.layers.Dense(1, activation='sigmoid')
         },
         preprocessing_combiner=kr.layers.Concatenate(axis=-1),
